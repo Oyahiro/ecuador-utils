9c2611bd0ac353ce2e49ff974e2c6d40c1e0db54
Christian Chunga <christian98.cb@outlook.es>
2024-09-22 22:58:14 -0500

    Add all cities

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,36 @@
 interface Province {
+    code: string;
     name: string;
     cities: string[];
 }
 
 const provincesList: Province[] = [
-    { name: "Azuay", cities: ["Cuenca", "Gualaceo", "Paute"] },
-    { name: "Pichincha", cities: ["Quito", "Cayambe", "Machachi"] },
-    { name: "Guayas", cities: ["Guayaquil", "Daule", "Samborondón"] },
-    // Agrega más provincias y ciudades aquí
+    { code: "01", name: "Azuay", cities: ["Cuenca", "Gualaceo", "Paute", "Girón", "Sigsig", "Nabón", "Oña", "Chordeleg", "El Pan", "Sevilla de Oro", "Guachapala", "Pucará"] },
+    { code: "02", name: "Bolívar", cities: ["Guaranda", "Chillanes", "Chimbo", "Echeandía", "San Miguel", "Caluma", "Las Naves"] },
+    { code: "03", name: "Cañar", cities: ["Azogues", "Biblián", "Cañar", "La Troncal", "El Tambo", "Déleg", "Suscal"] },
+    { code: "04", name: "Carchi", cities: ["Tulcán", "Bolívar", "Espejo", "Mira", "Montúfar", "San Pedro de Huaca"] },
+    { code: "05", name: "Chimborazo", cities: ["Riobamba", "Alausí", "Colta", "Chambo", "Chunchi", "Guamote", "Guano", "Pallatanga", "Penipe", "Cumandá"] },
+    { code: "06", name: "Cotopaxi", cities: ["Latacunga", "La Maná", "Pangua", "Pujilí", "Salcedo", "Saquisilí", "Sigchos"] },
+    { code: "07", name: "El Oro", cities: ["Machala", "Arenillas", "Atahualpa", "Balsas", "Chilla", "El Guabo", "Huaquillas", "Las Lajas", "Marcabelí", "Pasaje", "Piñas", "Portovelo", "Santa Rosa", "Zaruma"] },
+    { code: "08", name: "Esmeraldas", cities: ["Esmeraldas", "Atacames", "Eloy Alfaro", "Muisne", "Quinindé", "Rioverde", "San Lorenzo"] },
+    { code: "09", name: "Galápagos", cities: ["Puerto Baquerizo Moreno", "Puerto Ayora", "Puerto Villamil"] },
+    { code: "10", name: "Guayas", cities: ["Guayaquil", "Daule", "Samborondón", "Durán", "Milagro", "Naranjal", "Balao", "Balzar", "Colimes", "El Empalme", "El Triunfo", "General Villamil", "Isidro Ayora", "Lomas de Sargentillo", "Marcelino Maridueña", "Naranjito", "Palestina", "Pedro Carbo", "Playas", "Salitre", "Santa Lucía", "Simón Bolívar", "Yaguachi"] },
+    { code: "11", name: "Imbabura", cities: ["Ibarra", "Antonio Ante", "Cotacachi", "Otavalo", "Pimampiro", "San Miguel de Urcuquí"] },
+    { code: "12", name: "Loja", cities: ["Loja", "Calvas", "Catamayo", "Celica", "Chaguarpamba", "Espíndola", "Gonzanamá", "Macará", "Olmedo", "Paltas", "Pindal", "Puyango", "Saraguro", "Sozoranga", "Zapotillo"] },
+    { code: "13", name: "Los Ríos", cities: ["Babahoyo", "Baba", "Buena Fe", "Montalvo", "Palenque", "Puebloviejo", "Quevedo", "Quinsaloma", "Urdaneta", "Valencia", "Ventanas", "Vinces"] },
+    { code: "14", name: "Manabí", cities: ["Portoviejo", "Chone", "El Carmen", "Flavio Alfaro", "Jaramijó", "Jipijapa", "Junín", "Manta", "Montecristi", "Olmedo", "Paján", "Pedernales", "Pichincha", "Puerto López", "Rocafuerte", "Santa Ana", "Sucre", "Tosagua"] },
+    { code: "15", name: "Morona Santiago", cities: ["Macas", "Gualaquiza", "Limón Indanza", "Palora", "Santiago", "Sucúa", "Huamboya", "San Juan Bosco", "Taisha"] },
+    { code: "16", name: "Napo", cities: ["Tena", "Archidona", "El Chaco", "Quijos", "Carlos Julio Arosemena Tola"] },
+    { code: "17", name: "Pastaza", cities: ["Puyo", "Arajuno", "Mera", "Santa Clara"] },
+    { code: "18", name: "Pichincha", cities: ["Quito", "Cayambe", "Mejía", "Pedro Moncayo", "Pedro Vicente Maldonado", "Puerto Quito", "Rumiñahui", "San Miguel de los Bancos"] },
+    { code: "19", name: "Sucumbíos", cities: ["Nueva Loja", "Cascales", "Cuyabeno", "Gonzalo Pizarro", "Lago Agrio", "Putumayo", "Shushufindi", "Sucumbíos"] },
+    { code: "20", name: "Tungurahua", cities: ["Ambato", "Baños", "Cevallos", "Mocha", "Patate", "Pelileo", "Quero", "Tisaleo"] },
+    { code: "21", name: "Zamora Chinchipe", cities: ["Zamora", "Centinela del Cóndor", "Chinchipe", "El Pangui", "Nangaritza", "Palanda", "Paquisha", "Yacuambi", "Yantzaza"] },
+    { code: "22", name: "Santa Elena", cities: ["Santa Elena", "La Libertad", "Salinas"] },
+    { code: "23", name: "Santo Domingo de los Tsáchilas", cities: ["Santo Domingo", "La Concordia"] },
+    { code: "24", name: "Orellana", cities: ["Puerto Francisco de Orellana", "Aguarico", "La Joya de los Sachas", "Loreto"] }
 ];
+
 
 export const provinces = (): string[] => {
     return provincesList.map((province) => province.name);
@@ -16,7 +38,6 @@ export const provinces = (): string[] => {
 
 export const cities = (provinceName?: string): string[] => {
     if (!provinceName) {
-        // Si no se pasa provincia, devolver todas las ciudades del país.
         return provincesList.flatMap((province) => province.cities);
     }
     const province = provincesList.find(
